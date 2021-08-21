3b0c50312b46f6a8c3752ae3fed698db5df7021d
danghoski <danghoskinfotech@gmail.com>
2021-08-21 12:06:00 +0100

    Initial commit

---
+++ app/src/main/java/com/danghoski/foodicious/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.danghoski.foodicious
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
